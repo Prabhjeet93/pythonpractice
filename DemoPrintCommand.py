a1441d452823fc48589d1459992630ab632e68ad
Prabhjeet Singh <51075201+Prabhjeet93@users.noreply.github.com>
2022-06-12 22:44:09 -0400

    Create DemoPrintCommand.py

---
+++ DemoPrintCommand.py
@@ -0,0 +1,29 @@
+
+############ Demo of print command Code #####################
+
+
+#Demo of  print command
+print('Hello')                  # print hello word
+print("Prabhjeet Singh")        # print name
+print(0000000)                  # print zero, it will print only one zero.
+print("0000000")                # print multiple zeros.
+print(1111111)                  # print multiple ones.
+
+# Demo of print variable value with message
+st4 = "College"                 # Assigning a string value to a variable
+print("Loyalist",st4)           # printing a string variable with the message where variable is in the last
+st5 = "Loyalist " + st4         # Assigning a string value to a variable
+print(st5,"Belleville")         # printing a string variable with the message where variable is in the last
+
+
+name="jack"
+age=19
+print("name is "+ name   + " age is ", age)
+print (f"name is {name}                 age is {age}")
+
+student1 = input('Name: ')
+student2 = input('Name: ')
+
+print(student1)
+print(student2)
+
